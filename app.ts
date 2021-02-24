03af557017281a7384aee0e85084e352b8a0c173
Jaakko Mäkilaurila <jaakko.makilaurila@siili.com>
2021-02-24 21:33:38 +0200

    Add eslnt and fix errors + remove package-lock.json (using yarn.lock)

--- app.ts
+++ app.ts
@@ -2,8 +2,8 @@ import express from 'express'
 
 const app = express()
 
-app.get("/", (req, res) => {
-  res.send("Hello todo")
+app.get('/', (req, res) => {
+  res.send('Hello todo')
 })
 
 export default app
