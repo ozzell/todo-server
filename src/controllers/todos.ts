390ff7b65213862392fbbef7d6608c6cbc2e531a
Jaakko Mäkilaurila <jaakko.makilaurila@siili.com>
2021-02-28 23:12:24 +0200

    Move controllers to their own directory

---
+++ src/controllers/todos.ts
@@ -0,0 +1,47 @@
+import { Request, Response, NextFunction } from 'express'
+import Todo from '../models/todo'
+import { TodoSchema } from '../models/types'
+
+export const getTodos = (req: Request, res: Response, next: NextFunction) => {
+  Todo.find({})
+    .then(todos => {
+      res.json(todos)
+    })
+    .catch(error => next(error))
+}
+
+export const postTodo = (req: Request, res: Response, next: NextFunction) => {
+  const { text, done }: TodoSchema = req.body
+  const todo = new Todo({ text, done })
+  todo.save()
+    .then(savedTodo => {
+      res.status(201).json(savedTodo)
+    })
+    .catch(error => next(error))
+}
+
+export const markTodoDone = (req: Request, res: Response, next: NextFunction) => {
+  const { done } = req.body
+  Todo.findByIdAndUpdate(req.params.id, { done }, { new: true })
+    .then(result => {
+      if (result) {
+        res.json(result)
+      } else {
+        // TODO: Create resource if it doesn't exist (remember idempotency)
+        res.status(204).json()
+      }
+    })
+    .catch(error => next(error))
+}
+
+export const deleteTodo = (req: Request, res: Response, next: NextFunction) => {
+  Todo.findByIdAndDelete(req.params.id)
+    .then(result => {
+      if (result) {
+        res.json(result)
+      } else {
+        res.status(404).json({ error: 'todo not found' })
+      }
+    })
+    .catch(error => next(error))
+}
--- src/routes/todoRouter.ts
+++ src/routes/todoRouter.ts
@@ -1,51 +1,11 @@
 import { Router } from 'express'
-import Todo from '../models/todo'
-import { TodoSchema } from '../models/types'
+import { getTodos, postTodo, markTodoDone, deleteTodo } from '../controllers/todos'
 
 const todoRouter = Router()
 
-todoRouter.get('/', (req, res, next) => {
-  Todo.find({})
-    .then(todos => {
-      res.json(todos)
-    })
-    .catch(error => next(error))
-})
-
-todoRouter.post('/', (req, res, next) => {
-  const { text, done }: TodoSchema = req.body
-  const todo = new Todo({ text, done })
-  todo.save()
-    .then(savedTodo => {
-      res.status(201).json(savedTodo)
-    })
-    .catch(error => next(error))
-})
-
-todoRouter.put('/:id', (req, res, next) => {
-  const { done } = req.body
-  Todo.findByIdAndUpdate(req.params.id, { done }, { new: true })
-    .then(result => {
-      if (result) {
-        res.json(result)
-      } else {
-        // TODO: Create resource if it doesn't exist (remember idempotency)
-        res.status(204).json()
-      }
-    })
-    .catch(error => next(error))
-})
-
-todoRouter.delete('/:id', (req, res, next) => {
-  Todo.findByIdAndDelete(req.params.id)
-    .then(result => {
-      if (result) {
-        res.json(result)
-      } else {
-        res.status(404).json({ error: 'todo not found' })
-      }
-    })
-    .catch(error => next(error))
-})
+todoRouter.get('/', getTodos)
+todoRouter.post('/', postTodo)
+todoRouter.put('/:id', markTodoDone)
+todoRouter.delete('/:id', deleteTodo)
 
 export default todoRouter
