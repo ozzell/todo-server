94377f06396912a1e626c60034899653efefef8b
Jaakko Mäkilaurila <jaakko.makilaurila@siili.com>
2021-03-03 21:37:20 +0200

    Add cache

--- src/routes/todoRouter.ts
+++ src/routes/todoRouter.ts
@@ -1,9 +1,15 @@
-import { Router } from 'express'
+import { Router, Request, Response, NextFunction } from 'express'
+import apicache from 'apicache'
 import { getTodos, postTodo, markTodoDone, deleteTodo } from '../controllers/todos'
 
 const todoRouter = Router()
+const cache = apicache.middleware
 
-todoRouter.get('/', getTodos)
+const cacheIfNotInTest = (cacheTime: string) => process.env.NODE_ENV !== 'test'
+  ? cache(cacheTime)
+  : (req: Request, res: Response, next: NextFunction) => next()
+
+todoRouter.get('/', cacheIfNotInTest('5 minutes'), getTodos)
 todoRouter.post('/', postTodo)
 todoRouter.put('/:id', markTodoDone)
 todoRouter.delete('/:id', deleteTodo)
